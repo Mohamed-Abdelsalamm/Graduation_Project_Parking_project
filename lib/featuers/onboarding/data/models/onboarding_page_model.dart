5e7b6adc378415f0badcddfbfa3537b8e296aff4
Omar Elmesiry <omarmeciry21@gmail.com>
2023-12-20 15:32:46 +0200

    Completed OnBoarding Model & Repo + Working on OnBoarding UI

---
+++ lib/featuers/onboarding/data/models/onboarding_page_model.dart
@@ -0,0 +1,15 @@
+class OnBoardingPageModel {
+  String assetImage;
+  String titlePart1;
+  String titlePart2;
+  String titleKeyword;
+  String description;
+
+  OnBoardingPageModel({
+    required this.assetImage,
+    required this.titlePart1,
+    required this.titleKeyword,
+    required this.titlePart2,
+    required this.description,
+  });
+}
---
+++ lib/featuers/onboarding/data/repo/onboarding_repo.dart
@@ -0,0 +1,5 @@
+import 'package:parking/featuers/onboarding/data/models/onboarding_page_model.dart';
+
+abstract class OnBoardingRepo {
+  List<OnBoardingPageModel> getOnBoardingPages();
+}
